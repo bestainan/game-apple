2d2dbd337732d61efd83c1dbb06f477e8db18a79
Robot <a.robot.n@gmail.com>
2018-02-22 15:21:40 +0800

    no message

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ import * as actions from './action'
 Vue.use(Vuex);
 const state = {
     is_alived: true,
-    base_url: 'http://127.0.0.1:8000/',
+    base_url: 'http://tzadmin.jiuxingjinfu.cn/',
     error_msg: '',
     show_error_msg: false,
     user:{
