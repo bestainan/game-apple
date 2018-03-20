c0d0e3b48e662b31f162d304cb3b9d3e25621d03
robot <a.robot.n@gmail.com>
2018-03-20 22:56:16 +0800

    no message

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,8 @@ import * as actions from './action'
 Vue.use(Vuex);
 const state = {
     is_alived: true,
-    base_url: 'http://127.0.0.1:8000/',
-    // base_url: 'http://tzadmin.jiuxingjinfu.cn/',
+    // base_url: 'http://127.0.0.1:8000/',
+    base_url: 'http://tzadmin.jiuxingjinfu.cn/',
     show_menu:'',
     error_msg: '',
     show_error_msg: false,
